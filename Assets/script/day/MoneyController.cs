8769a26f88967b07340755182a4896a68599f126
sjyasj <53181373+sjyasj@users.noreply.github.com>
2019-08-10 16:40:31 +0900

    임시

--- Assets/script/day/MoneyController.cs
+++ Assets/script/day/MoneyController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class WorkerText
+{
+    public List<Text> people;
+}
+
 public class MoneyController : MonoBehaviour
 {
     int money = 450000;
     int month = 1;
     int monthlyPay = 45000;
     List<int> people = new List<int> { 0, 15, 0 };
-    int ok = 0;
+    int okh = 0;
+    int okf = 0;
     int outcomeDay;
 
     public Text myMoney;
@@ -17,6 +24,7 @@ public class MoneyController : MonoBehaviour
     public Text MonthlyPayText;
     public List<Text> peopleText;
     public Text OutcomeDayText;
+    public List<WorkerText> test;
 
     private float time;
 
@@ -40,23 +48,46 @@ public class MoneyController : MonoBehaviour
     }
     public void Hire()
     {
-        ok = 1;
+        okh = 1;
     }
 
-    public void MonthPay(int pay)
+    public void MonthPayH(int pay)
     {
-        if (ok==1)
+        if (okh==1)
         {
             monthlyPay += pay;
         }
     }
-    public void People(int peo)
+    public void PeopleH(int peo)
     {
-        if (ok == 1)
+        if (okh == 1)
         {
             people[peo] += 1;
             peopleText[peo].text = people[peo].ToString() + " 명";
-            ok = 0;
+            okh = 0;
+        }
+    }
+    public void Fire(int peo)
+    {
+        if(people[peo]>0)
+        {
+            okf = 1;
+        }
+    }
+    public void MonthPayF(int pay)
+    {
+        if (okf == 1)
+        {
+            monthlyPay -= pay;
+        }
+    }
+    public void PeopleF(int peo)
+    {
+        if (okf == 1)
+        {
+            people[peo] -= 1;
+            peopleText[peo].text = people[peo].ToString() + " 명";
+            okf = 0;
         }
     }
     public void Outcome()
