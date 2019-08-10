776cda8ec881a7b1b933b8a3b9d8fa351ac8c09e
sjyasj <53181373+sjyasj@users.noreply.github.com>
2019-08-10 15:27:00 +0900

    직원 고용 코드 작성

--- Assets/script/day/MoneyController.cs
+++ Assets/script/day/MoneyController.cs
@@ -5,46 +5,69 @@ using UnityEngine.UI;
 
 public class MoneyController : MonoBehaviour
 {
-    int money = 5000;
+    int money = 450000;
     int month = 1;
-    int monthlyPay = 0;
-    int monthlyGain = 0;
+    int monthlyPay = 45000;
+    List<int> people = new List<int> { 0, 15, 0 };
+    int ok = 0;
+    int outcomeDay;
 
     public Text myMoney;
     public Text date;
     public Text MonthlyPayText;
-    public Text MonthlyGainText;
+    public List<Text> peopleText;
+    public Text OutcomeDayText;
+
+    private float time;
 
     private void Update()
     {
         myMoney.text = "자본 : " + money.ToString();
         MonthlyPayText.text = "지출 : " + monthlyPay.ToString();
-        MonthlyGainText.text = "수입 : " + monthlyGain.ToString();
-    }
 
-    public void NextDay()
-    {
-        /*money += Random.Range(400, 601);*/
-        money += monthlyGain;
-        money -= monthlyPay;
-        month += 1;
-        date.text = month.ToString() + "월";
-    }
+        OutcomeDayText.text = (outcomeDay - ShortenMonth()).ToString() + "월 남음";
 
-    public void Hire(int price)
-    {
-        if (money >= price)
+        time += Time.deltaTime;
+        if (time >= 5f)
         {
-            money -= price;
+            money -= monthlyPay;
+            outcomeDay -= 1;
+            money -= 10000;
+            month += 1;
+            date.text = month.ToString() + "월";
+            time = 0;
         }
+    }
+    public void Hire()
+    {
+        ok = 1;
     }
 
     public void MonthPay(int pay)
     {
-        monthlyPay += pay;
+        if (ok==1)
+        {
+            monthlyPay += pay;
+        }
     }
-    public void MonthGain(int gain)
+    public void People(int peo)
     {
-        monthlyGain += gain;
+        if (ok == 1)
+        {
+            people[peo] += 1;
+            peopleText[peo].text = people[peo].ToString() + " 명";
+            ok = 0;
+        }
+    }
+    public void Outcome()
+    {
+        outcomeDay = money / 9000;
+    }
+
+    private int ShortenMonth()
+    {
+        int month = (people[0] / 2) + (people[1] / 10) + (people[2] / 30);
+
+        return month;
     }
 }
