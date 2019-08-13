ddae6cd94dd5ee2e4ca9f640562cb72d3735ebb1
EQwon <bluetwintail0906@gmail.com>
2019-08-13 15:01:21 +0900

    코드 리팩토링 중

---
+++ Assets/Scripts/Business.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Business
+{
+    public string name; //부서 이름
+    public List<int> worker; // 직원수 { 상급, 중급, 하급 }
+
+    private int period; //개발 기간
+    public int Period { get { return period < 0 ? 0 : period - DecreasedPeriodByWorker(); } }
+
+    public void Initialize(DepartmentName departmentName)
+    {
+        name = departmentName.ToString();
+        worker = new List<int> { 0, 15, 0 };
+
+        period = -1;
+    }
+
+    public void HireWorker(int i)
+    {
+        worker[i] += 1;
+    }
+
+    public void FireWorker(int i)
+    {
+        worker[i] -= 1;
+    }
+
+    public void StartDevelop()
+    {
+        int nowMoney = GameManager.instance.Money;
+
+        GameManager.instance.UseMoney(nowMoney / 12);
+        period = nowMoney / 9000;
+    }
+
+    private int DecreasedPeriodByWorker()
+    {
+        return (worker[0] / 7) + (worker[1] / 15) + (worker[2] / 30);
+    }
+
+    public int MonthlyPay()
+    {
+        return worker[0] * 6000 + worker[1] * 3000 + worker[2] * 2000;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum DepartmentName { 식품, 운송, 전자, 통신, 화학, 스포츠, 의류, 디스플레이 };
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public List<Business> department = new List<Business> { };
+
+    private int day;
+    private float time;
+
+    private int money;
+    public int Money { get { return money; } }
+
+    [Header("UI Holder")]
+    public Text DateText;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        day = 1;
+        time = 0;
+        money = 45000;
+
+        CreateNewDepartment(DepartmentName.식품);
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= 5f)
+        {
+            day += 1;
+            time = 0;
+        }
+    }
+
+    public void UseMoney(int amount)
+    {
+        money -= amount;
+    }
+
+    public void CreateNewDepartment(DepartmentName name)
+    {
+        Business business = new Business();
+        business.Initialize(name);
+        department.Add(business);
+    }
+}
